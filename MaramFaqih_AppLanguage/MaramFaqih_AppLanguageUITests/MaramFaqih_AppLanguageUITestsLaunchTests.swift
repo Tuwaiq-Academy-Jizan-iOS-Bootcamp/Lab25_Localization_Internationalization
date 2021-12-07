53caf3941b89125d105474c38c267b62705fdc8e
maram <maramfaqih92@gmail.com>
2021-12-07 20:06:30 +0300

    Lab25 : Maram Faqih

---
+++ MaramFaqih_AppLanguage/MaramFaqih_AppLanguageUITests/MaramFaqih_AppLanguageUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MaramFaqih_AppLanguageUITestsLaunchTests.swift
+//  MaramFaqih_AppLanguageUITests
+//
+//  Created by meme f on 03/05/1443 AH.
+//
+
+import XCTest
+
+class MaramFaqih_AppLanguageUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
