e05583bdb24eb5fd0c8124936bcab0c18214aafa
ahlamjur <ahlamjur@gmail.com>
2021-12-09 09:23:40 +0300

    lab25

---
+++ lab6Dec/lab6DecUITests/lab6DecUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  lab6DecUITestsLaunchTests.swift
+//  lab6DecUITests
+//
+//  Created by Ahlam Ahlam on 02/05/1443 AH.
+//
+
+import XCTest
+
+class lab6DecUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
